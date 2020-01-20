5a6506add215577488b36922f2ccc099809d6c2f
smilemango <smileman.god@gmail.com>
2020-01-20 17:59:28 +0900

    unit test (django with pytest)

---
+++ products/tests/test_views.py
@@ -0,0 +1,35 @@
+from django.test import RequestFactory
+from django.urls import reverse
+from django.contrib.auth.models import User,AnonymousUser
+from products.views import product_detail
+from mixer.backend.django import mixer
+from django.test import TestCase
+import pytest
+
+@pytest.mark.django_db
+class TestViews(TestCase):
+
+  @classmethod
+  def setUpClass(cls):
+    super(TestViews, cls).setUpClass()
+    mixer.blend('products.Product')
+    cls.factory = RequestFactory();
+
+  def test_product_detail_authenticated(self):
+    path = reverse('detail',kwargs={'pk':1})
+    request= self.factory.get(path)
+    request.user = mixer.blend(User)
+
+    response = product_detail(request, pk=1)
+    assert response.status_code == 200
+
+
+  def test_product_detail_unauthenticated(self):
+
+    path = reverse('detail',kwargs={'pk':1})
+    request= self.factory.get(path)
+    request.user = AnonymousUser()
+
+    response = product_detail(request, pk=1)
+    assert 'accounts/login' in response.url
+
